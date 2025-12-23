84979998a85f792a888d4438a6f1ae5c6afb7d64
MazumalaAja <programlia1108@gmail.com>
2025-12-23 21:13:38 +0700

    create To-do-list project

---
+++ Modules/server/server.php
@@ -0,0 +1,7 @@
+<?php
+// Server merupakan global variable yang mengandung banyak informasi server 
+// foreach ($_SERVER as $data => $item) {
+//      echo "<br> $data : $item";
+// }
+
+echo $_SERVER[""];
