f209e4d24c3e780d50e19921c76c1950f31c1e90
MazumalaAja <programlia1108@gmail.com>
2025-12-26 06:40:45 +0700

    Modify module-12

--- Modules/module-12.php
+++ Modules/module-12.php
@@ -1,2 +1,34 @@
 <?php
 // Inheritance
+// pewarisan , sama seperti namanya jadi class  bisa mewariskan method ke class lain , yang di wariskan namanya sub class dan yang diwarisi disebut parent class
+
+class Animal
+{
+     protected $name;
+     protected $type;
+     protected $place;
+     private $voice;
+
+     public function __construct($name, $type, $place)
+     {
+          $this->name = $name;
+          $this->type = $type;
+          $this->place = $place;
+     }
+
+     public function setVoice($params)
+     {
+          $this->voice = $params;
+     }
+
+     public function getVoice()
+     {
+          return $this->voice;
+     }
+}
+
+class Mamalia extends Animal {}
+
+$cow = new Mamalia("Cow", "4 legs", "field");
+$cow->setVoice("Mooo");
+echo $cow->getVoice();
