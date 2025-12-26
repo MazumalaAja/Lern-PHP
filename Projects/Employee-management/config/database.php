18839e6f58d47b1265fa419de61339abe1d86d26
MazumalaAja <programlia1108@gmail.com>
2025-12-26 21:23:50 +0700

    continue the project employee-management

---
+++ Projects/Employee-management/config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$dbname = "employee_management";
+$username = "root";
+$password = "mazumala21";
+
+try {
+     // Membuat koneksi dengan database...
+     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+     // Intinya ini untuk mengambil error jika ada kesalahan koneksi
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     echo "Koneksi berhasil";
+} catch (PDOException $e) {
+     die("Koneksi database gagal : " . $e->getMessage());
+}
