2af72e75f6b083282ae617d0bf52074a8b4f8fa3
MazumalaAja <programlia1108@gmail.com>
2025-12-27 15:16:17 +0700

    complete project management-employee

--- Projects/Employee-management/include/layout.php
+++ Projects/Employee-management/include/layout.php
@@ -4,6 +4,7 @@ require_once "../config/database.php";
 // Get data from database
 $database = $pdo->prepare("SELECT * FROM employees");
 $database->execute();
+$employees = $database->fetchAll(PDO::FETCH_ASSOC);
 
 // query functions...
 function querySet($query, $datas = [])
@@ -15,13 +16,28 @@ function querySet($query, $datas = [])
 
 // if button add clicked...
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     if (isset($_POST["username"]) && isset($_POST["address"]) && isset($_POST["salary"])) {
+     if (isset($_POST["add"])) {
           $username = $_POST["username"];
           $address = $_POST["address"];
           $salary = $_POST["salary"];
           querySet("INSERT INTO employees (username,address,salary) VALUES (:username,:address,:salary)", ['username' => $username, 'address' => $address, 'salary' => $salary]);
           header("Location: " . $_SERVER["PHP_SELF"]);
           exit;
+     } elseif (isset($_POST["update"])) {
+          $id = $_POST["id"];
+          $username = $_POST["username"];
+          $address = $_POST["address"];
+          $salary = $_POST["salary"];
+          querySet("UPDATE  employees SET username = :username , address= :address , salary = :salary WHERE id = :id", ['id' => $id, 'username' => $username, 'address' => $address, 'salary' => $salary]);
+          header("Location: " . $_SERVER["PHP_SELF"]);
+          exit;
+     } elseif (isset($_POST["remove"])) {
+          $id = $_POST["id"];
+          querySet("DELETE FROM employees WHERE id = :id", ['id' => $id]);
+          header("Location: " . $_SERVER["PHP_SELF"]);
+          exit;
+     } else {
+          return null;
      }
 }
 
@@ -43,6 +59,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           <button class="btn btn-primary" style="width: max-content;" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Employee</button>
           <?php include "./modal.php"; ?>
           <div class="bg-white rounded-3 shadow-sm p-3">
+               <?php foreach ($employees as $data): ?>
+                    <!-- Modal -->
+                    <div class="modal fade" id="editModal<?php echo $data['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                         <div class="modal-dialog modal-lg bg-white rounded-3">
+                              <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post" class="modal-content">
+                                   <input type="hidden" name="id" value="<?= $data['id'] ?>">
+                                   <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Update</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                   </div>
+                                   <div class="modal-body">
+                                        <div>
+                                             <div class="mb-3">
+                                                  <label class="form-label" for="username">Username</label>
+                                                  <input value="<?= $data["username"] ?>" name="username" id="username" type="text" class="form-control" placeholder="Enter employee's name..." required>
+                                             </div>
+                                             <div class="mb-3">
+                                                  <label class="form-label" for="address">Address</label>
+                                                  <input value="<?= $data["address"] ?>" name="address" id="address" type="text" class="form-control" placeholder="Enter employee's address..." required>
+                                             </div>
+                                             <div class="mb-3">
+                                                  <label class="form-label" for="salary">Salary</label>
+                                                  <input value="<?= $data["salary"] ?>" name="salary" id="salary" type="number" class="form-control" placeholder="Enter employee's salary..." required>
+                                             </div>
+                                        </div>
+                                   </div>
+                                   <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button name="update" type="submit" class="btn btn-primary">Save changes</button>
+                                   </div>
+                              </form>
+                         </div>
+                    </div>
+               <?php endforeach; ?>
                <table class="table">
                     <thead>
                          <tr>
@@ -54,15 +104,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                          </tr>
                     </thead>
                     <tbody>
-                         <?php foreach ($database as $data): ?>
+                         <?php foreach ($employees as $data): ?>
                               <tr>
                                    <td><?= $data["id"] ?></td>
                                    <td><?= $data["username"] ?></td>
                                    <td><?= $data["address"] ?></td>
                                    <td>Rp <?= number_format($data["salary"], 0, ",", ".") ?></td>
-                                   <td>
-                                        <button class="btn btn-success">Edit</button>
-                                        <button class="btn btn-danger">Deleete</button>
+                                   <td class="d-flex gap-3 align-items-center">
+                                        <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editModal<?php echo $data['id']; ?>">Edit</button>
+                                        <form method="post" action="<?= $_SERVER["PHP_SELF"] ?>">
+                                             <input name="id" type="hidden" value="<?= $data['id'] ?>">
+                                             <button type="submit" name="remove" class="btn btn-danger">Delete</button>
+                                        </form>
                                    </td>
                               </tr>
                          <?php endforeach; ?>
--- Projects/Employee-management/include/modal.php
+++ Projects/Employee-management/include/modal.php
@@ -1,7 +1,7 @@
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
      <div class="modal-dialog modal-lg">
-          <form action="<?php $_SERVER["PHP_SELF"] ?>" method="post" class="modal-content">
+          <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post" class="modal-content">
                <div class="modal-header">
                     <h1 class="modal-title fs-5" id="exampleModalLabel">Add new employee</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -10,21 +10,21 @@
                     <div>
                          <div class="mb-3">
                               <label class="form-label" for="username">Username</label>
-                              <input name="username" id="username" type="text" class="form-control" placeholder="Enter employee's name...">
+                              <input name="username" id="username" required type="text" class="form-control" placeholder="Enter employee's name...">
                          </div>
                          <div class="mb-3">
                               <label class="form-label" for="address">Address</label>
-                              <input name="address" id="address" type="text" class="form-control" placeholder="Enter employee's address...">
+                              <input name="address" required id="address" type="text" class="form-control" placeholder="Enter employee's address...">
                          </div>
                          <div class="mb-3">
                               <label class="form-label" for="salary">Salary</label>
-                              <input name="salary" id="salary" type="number" class="form-control" placeholder="Enter employee's salary...">
+                              <input name="salary" required id="salary" type="number" class="form-control" placeholder="Enter employee's salary...">
                          </div>
                     </div>
                </div>
                <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button name="add" type="submit" class="btn btn-primary">Save changes</button>
+                    <button name="add" type="submit" class="btn btn-primary">Add Data</button>
                </div>
           </form>
      </div>
